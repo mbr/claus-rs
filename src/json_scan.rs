79befd56c48e15356e8df64b4520459e8d12b121
Marc Brinkmann <git@marcbrinkmann.de>
2025-06-04 11:59:53 +0200

    Remove tests from JSON scan

--- src/json_scan.rs
+++ src/json_scan.rs
@@ -1,1 +1,75 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum ScanState {
+    LookingForStart,
+    InObject,
+    InString,
+    InEscape,
+}
 
+#[derive(Debug, PartialEq)]
+pub enum ScanResult {
+    NeedsMore,
+    Error,
+    Found(usize), // Split point in the input slice
+}
+
+pub struct JsonScanner {
+    state: ScanState,
+    brace_depth: usize,
+}
+
+impl JsonScanner {
+    pub fn new() -> Self {
+        Self {
+            state: ScanState::LookingForStart,
+            brace_depth: 0,
+        }
+    }
+
+    pub fn scan(&mut self, input: &[u8]) -> ScanResult {
+        for (i, &byte) in input.iter().enumerate() {
+            match self.state {
+                ScanState::LookingForStart => {
+                    if byte.is_ascii_whitespace() {
+                        continue;
+                    } else if byte == b'{' {
+                        self.state = ScanState::InObject;
+                        self.brace_depth = 1;
+                    } else {
+                        return ScanResult::Error;
+                    }
+                }
+
+                ScanState::InObject => match byte {
+                    b'{' => self.brace_depth += 1,
+                    b'}' => {
+                        self.brace_depth -= 1;
+                        if self.brace_depth == 0 {
+                            self.reset();
+                            return ScanResult::Found(i + 1);
+                        }
+                    }
+                    b'"' => self.state = ScanState::InString,
+                    _ => {}
+                },
+
+                ScanState::InString => match byte {
+                    b'"' => self.state = ScanState::InObject,
+                    b'\\' => self.state = ScanState::InEscape,
+                    _ => {}
+                },
+
+                ScanState::InEscape => {
+                    self.state = ScanState::InString;
+                }
+            }
+        }
+
+        ScanResult::NeedsMore
+    }
+
+    fn reset(&mut self) {
+        self.state = ScanState::LookingForStart;
+        self.brace_depth = 0;
+    }
+}
