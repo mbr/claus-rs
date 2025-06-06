e15b3c5375f78ec416216d8b4685b95847d4e8ca
Marc Brinkmann <git@marcbrinkmann.de>
2025-06-06 23:04:42 +0200

    Add clippy on examples

--- check.sh
+++ check.sh
@@ -6,3 +6,6 @@ export RUSTFLAGS="-D warnings"
 cargo check --all-features
 cargo clippy
 cargo clippy --all-features --tests
+cargo clippy --example assistant --features=reqwest-blocking
+cargo clippy --example simple_chat --features=reqwest-blocking
+cargo clippy --example streaming --features=reqwest
